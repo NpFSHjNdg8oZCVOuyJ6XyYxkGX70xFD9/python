ba4a847707ee2616352741c4ba6aee2ace484743
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-08-27 19:59:43 +0000

    messafe

--- python/python.py
+++ python/python.py
@@ -4,4 +4,4 @@ money_avaliable=int(input("Money owned: ")) #input
 cases_bought=int(input("Cases bought: "))   #input
 cost_per_case=int(input("Price of case: "))  #input
 money_left=money_avaliable-cases_bought*cost_per_case
-print("Remaining money: ", money_left)  #output323
+print("Remaining money: ", money_left)  #output
