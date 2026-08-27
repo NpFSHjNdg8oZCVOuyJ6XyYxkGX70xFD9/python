7e00b637c9b40be001087a20da6fb10a6771362c
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-08-27 20:44:33 +0000

    message

---
+++ python/sequence.py
@@ -0,0 +1,16 @@
+scores = [72, 85, 91, 68, 88]
+title = "weekly score report"
+
+#print(scores[0], scores[3], scores[4])
+
+#scores[1]=86
+#print(scores)
+
+#scores.append(93)
+#print(scores)
+
+#print(title[0:6])
+#print(title[12:19])
+
+#new_title="new"+title[12:]
+print(new_title)
