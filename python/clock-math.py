888b69a596121fcd3d3a28113b8a0d49ecd42991
Reyaan Kalsheker <reyaan.kalsheker29@student.tamdistrict.org>
2026-09-03 04:57:53 +0000

    complete day 7

---
+++ python/clock-math.py
@@ -0,0 +1,44 @@
+clock_value=45
+remaining = clock_value
+
+bit_1=remaining %2
+remaining=remaining//2
+bit_2=remaining %2
+remaining=remaining//2
+bit_4=remaining %2
+remaining=remaining//2
+bit_8=remaining %2
+remaining=remaining//2
+bit_16=remaining %2
+remaining=remaining//2
+bit_32=remaining %2
+remaining=remaining//2
+
+clock_values=[13,42]
+labels=["hours","minutes"]
+clock_values.append(17)
+labels.append("seconds")
+selected_index=0
+clock_value=clock_values[selected_index]
+label=labels[selected_index]
+
+remaining = clock_value
+bit_1=remaining %2
+remaining=remaining//2
+bit_2=remaining %2
+remaining=remaining//2
+bit_4=remaining %2
+remaining=remaining//2
+bit_8=remaining %2
+remaining=remaining//2
+bit_16=remaining %2
+remaining=remaining//2
+bit_32=remaining %2
+remaining=remaining//2
+
+bits=[bit_32,bit_16,bit_8,bit_4,bit_2,bit_1]
+bit_text = str(bits[0]) + str(bits[1]) + str(bits[2]) + str(bits[3]) + str(bits[4]) + str(bits[5])
+check_value = bits[0] * 32 + bits[1] * 16 + bits[2] * 8 + bits[3] * 4 + bits[4] * 2 + bits[5] * 1
+print(str(label) + " : " + str(check_value) + " --> " + str(bit_text))
+print("Original : ", clock_value )
+print("Reconstructed: ", check_value)
